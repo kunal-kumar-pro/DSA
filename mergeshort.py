596e9598c65d2b40348872fd455be69cb7a4e3bc
kunal Kumar <kunal.kumar.professional@gmail.com>
2024-08-24 16:09:28 +0530

    Removed Dobule File for Merge Short

--- mergeshort.py
+++ mergeshort.py
@@ -24,41 +24,73 @@
 # shorted_array = merge_short(arr)
 # print(shorted_array)
 
-def merge (arr,low,mid,high):
-    left = arr[low:mid+1]
-    right = arr[mid+1:high+1]
-    i = j = 0
-    k = low
-    while i < len(left) and j < len(right) :
-        if left[i] < right[j] :
-            arr[k] = left[i]
-            k+=1
-            i+=1
-        else :
-            arr[k] = right[j]
-            k+=1
-            j+=1
-    while i < len(left) :
-        arr[k] = left[i]
-        k+=1
-        i+=1
-    while j < len(right) :
-        arr[k] = right[j]
-        k+=1
-        j+=1
+# def merge (arr,low,mid,high):
+#     left = arr[low:mid+1]
+#     right = arr[mid+1:high+1]
+#     i = j = 0
+#     k = low
+#     while i < len(left) and j < len(right) :
+#         if left[i] < right[j] :
+#             arr[k] = left[i]
+#             k+=1
+#             i+=1
+#         else :
+#             arr[k] = right[j]
+#             k+=1
+#             j+=1
+#     while i < len(left) :
+#         arr[k] = left[i]
+#         k+=1
+#         i+=1
+#     while j < len(right) :
+#         arr[k] = right[j]
+#         k+=1
+#         j+=1
 
 
 
-def merge_Short (arr,low,high) :
-    if low >= high :
-        return
-    mid = (low + high)//2
-    merge_Short(arr,low,mid)
-    merge_Short(arr,mid+1,high)
-    merge(arr,low,mid,high)
+# def merge_Short (arr,low,high) :
+#     if low >= high :
+#         return
+#     mid = (low + high)//2
+#     merge_Short(arr,low,mid)
+#     merge_Short(arr,mid+1,high)
+#     merge(arr,low,mid,high)
 
 
 
-arr = [34,7,23,32,5,62]
-shorted_array = merge_Short(arr,0,len(arr)-1)
-print(arr)
+# arr = [34,7,23,32,5,62]
+# shorted_array = merge_Short(arr,0,len(arr)-1)
+# print(arr)
+
+# def merge_sort(arr):
+#     if len(arr) <= 1:
+#         return arr
+
+#     mid = len(arr) // 2
+#     left_half = merge_sort(arr[:mid])
+#     right_half = merge_sort(arr[mid:])
+
+#     return merge(left_half, right_half)
+
+# def merge(left, right):
+#     sorted_list = []
+#     i = j = 0
+
+#     while i < len(left) and j < len(right):
+#         if left[i] < right[j]:
+#             sorted_list.append(left[i])
+#             i += 1
+#         else:
+#             sorted_list.append(right[j])
+#             j += 1
+
+#     # Append remaining elements
+#     sorted_list.extend(left[i:])
+#     sorted_list.extend(right[j:])
+
+#     return sorted_list
+
+# arr = [34, 7, 23, 32, 5, 62]
+# sorted_arr = merge_sort(arr)
+# print(sorted_arr)
